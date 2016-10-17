ef300c558c8182035a66af6f43b5d4fbf98b11e8
manueljun17 <manuelmanansalajr@gmail.com>
2016-10-17 12:45:39 +0800

    refactor homepage

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -15,7 +15,8 @@
         color:white;         
     }
     .banner {
-        position:absolute;
+      
+        position: relative;
         top:0;        
         z-index:-1;  
     }
@@ -46,6 +47,30 @@
         position:relative;
     }
 
+
+    /*-- Content -- */
+
+    /*-- Mission and Vision -- */
+    .mission-vision {
+        text-align: center;
+        background-color:#eee;
+    }
+    .mv-header{
+        font-size: 3em; 
+    }
+    .mv-content{
+        font-size: 1.3em;
+    }
+    /*-- Parish Officers -- */
+    .parish-officers .po-header{
+        text-align: center;
+    }
+    .po-header{
+        font-size: 3em; 
+    }
+    .po-content{
+        font-size: 1.3em;
+    }    
 </style>
 
 
@@ -78,11 +103,34 @@
                 <img src="img/1.jpg">
             </div>
         </header>
+
+        
         <figure class="main-content">
-            <h1>Vision and Mission of the Parish</h1>
+            <div class="mission-vision">
+                <div class="mv-header">
+                    <p>Mission and Vision of the Parish</p>
+                </div>
+                <div class="mv-content">
+                    <p>
+                    Over the past years, we have seen God do amazing things through ImmaconAngelesCityPH church to change lives in our community, our nation, and around the world. We are blown away at how God continues to strengthen and build the church and use us as a body of believers to bring the good news of the Gospel to people who need Jesus! This year we unite around the calling to be Christ's hands extended by loving with extravagance, serving with genuineness, and welcoming people home.
+                    </p>
+                    <button class="btn btn-info">NEED HELP?</button>
+                </div>
+            </div> 
+            <div class="parish-officers">
+                <div class="po-header">
+                    <p>Parish Officers</p>
+                </div>
+                <div class="po-content">
+                    <p>                    
+                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc laoreet, ligula ut lobortis commodo, enim ligula volutpat justo, sit amet pretium massa sem vitae lorem. Curabitur egestas rutrum dolor, sed consectetur justo convallis eu. Donec eros erat, placerat in scelerisque vitae, tristique fermentum nibh. Nunc congue hendrerit luctus. Proin accumsan molestie mi a iaculis. Vivamus nulla ligula, suscipit lacinia bibendum tempor, faucibus nec magna. Donec auctor metus leo, quis rhoncus erat viverra sit amet. Donec nec tempor libero.
+                    </p>
+                </div>
+            </div>    
         </figure>
 
     </div>
+
 	<script src="https://code.jquery.com/jquery.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </body>
