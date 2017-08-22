c4d3b115f760deccee7f152de4a4307fb65e8b16
manueljun17 <manueljun17@gmail.com>
2017-08-22 17:45:37 +0800

    update design on organization

--- resources/views/organizations/visitor/index.blade.php
+++ resources/views/organizations/visitor/index.blade.php
@@ -14,26 +14,33 @@
 </div>
 
 @if ($organizations->count())
-<div class="card-columns">
-     @foreach ($organizations as $organization)
-        <div class="card">
-            <?php
-             if(preg_match('/<img.+src=[\'"](?P<src>.+?)[\'"].*>/i', $organization->description)) preg_match('/<img.+src=[\'"](?P<src>.+?)[\'"].*>/i', $organization->description, $image);
-             else $image['src']='image/settings/event-default.jpg';  
-             ?>
-            <img class="card-img-top" src="{{ asset( $image['src']) }}" alt="Organization Image"  width="100%" height="auto">
-            <div class="card-block">
-                <h4 class="card-title">{{ $organization->name }}</h4>
-                <a href="{{ route('organizations.show',array($organization->id)) }}" class="btn btn-info">Show</a>
-            </div>
-        </div>
-    @endforeach
+<table class="table table-striped table-bordered table-responsive">
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Action</th>
+        </tr>
+    </thead>
+
+    <tbody>
+        @foreach ($organizations as $organization)
+            <tr>
+                <td>{{ $organization->id }}</td>
+                <td>{{ $organization->name }}</td>
+                <td>
+                    <a class="btn btn-info" href="{{ route('organizations.show',array($organization->id)) }}">Show</a>
+                </td>
+            </tr>
+        @endforeach
+    </tbody>
+  
+</table>
+<div class="text-center">
+<nav>
+  {!! $organizations->appends( Request::query() )->render() !!}
+</nav>
 </div>
-    <div class="text-center">
-    <nav>
-      {!! $organizations->appends( Request::query() )->render() !!}
-    </nav>
-  </div>
 @else
     There are no Organizations info
 @endif
